dc3c8190138cb9d25cf98fd103174aa371551e97
najwa820 <najwa.khairunnisa1300@student.unri.ac.id>
2026-06-04 23:04:12 +0700

    Lagi

--- GAME BALAP KARTU ANGKA/utils/constants.py
+++ GAME BALAP KARTU ANGKA/utils/constants.py
@@ -1,4 +1,4 @@
 PANJANG_JALUR = 35  # Panjang jalur permainan
 JUMLAH_KARTU = 6    # Banyak kartu yang digunakan
 BATAS_HISTORY = 6   # Batas maks data history yang disimpan
-KODE_HISTORY = 0    # Untuk menampilkan history permainan
+KODE_HISTORY = 0    # Kode untuk menampilkan history permainan
