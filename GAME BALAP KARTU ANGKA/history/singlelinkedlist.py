f9dc46b1fb858ce763bcf35301ed925ef05c0ec2
asyifasalsabila1 <asyifa.salsabila1750@student.unri.ac.id>
2026-06-06 20:58:24 +0800

    Update

--- GAME BALAP KARTU ANGKA/history/singlelinkedlist.py
+++ GAME BALAP KARTU ANGKA/history/singlelinkedlist.py
@@ -1,29 +1,29 @@
-class Node:
-    def __init__(self, isiHistory):
-        self.isiHistory = isiHistory
-        self.sambung = None
+class Node:                   #class node untuk menyimpan satu data history 
+    def __init__(self, isiHistory):     #inisialisasi node baru
+        self.isiHistory = isiHistory     #menyimpan isi histroy permainan
+        self.sambung = None              #menyimpan alamat node berikutnya
 
-class SingleLinkedList:
-    def __init__(self):
-        self.awal = None
+class SingleLinkedList:             #class single linked list
+    def __init__(self):             #inisialisasi linked list kosong
+        self.awal = None            #membuat node baru
     
-    def tambah_history(self, hasilGame):
-        rondeBaru = Node(hasilGame)
-        if self.awal is None:
-            self.awal = rondeBaru
+    def tambah_history(self, hasilGame):      #menambah history  baru ke linked list
+        rondeBaru = Node(hasilGame)           #membuat node baru
+        if self.awal is None:                 #mengecek apakah linked list kosong
+            self.awal = rondeBaru             #node baru menjadi node pertama
         else:
-            bantu = self.awal
-            while bantu.sambung is not None:
-                bantu = bantu.sambung
-            bantu.sambung = rondeBaru
+            bantu = self.awal                  #variabel baru mulai dari node pertama
+            while bantu.sambung is not None:  #mencari node terakhir
+                bantu = bantu.sambung         #pindah ke node berikutnya
+            bantu.sambung = rondeBaru         #menyambungkan  node trakhir dengan node baru
 
-    def tampilkan_history(self):
-        bantu = self.awal
-        nomor = 1
-        if self.awal is None:
-            print("History kosong")
+    def tampilkan_history(self):             #menampilkan seluruh history permainan
+        bantu = self.awal                    #variabel bantuk untuk menelusuri linkedlist
+        nomor = 1                            #nomor urut histroy
+        if self.awal is None:                #mengecek apakah linked list kosong
+            print("History kosong")          #menampilkan pesan jika tidak ada history
         else:
-            while bantu is not None:
-                print(f"{nomor}. {bantu.isiHistory}")
-                bantu = bantu.sambung
-                nomor += 1
+            while bantu is not None:         #menampilkan seluruh data history
+                print(f"{nomor}. {bantu.isiHistory}")   #menampilkan nomor dan isi history
+                bantu = bantu.sambung         #pindah ke node berikutnya
+                nomor += 1                     #menambah nomor urut
