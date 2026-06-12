b496cd288042677ff903840d78512a95eca40c67
najwa820 <najwa.khairunnisa1300@student.unri.ac.id>
2026-06-12 08:03:32 +0700

    Update

--- GAME BALAP KARTU ANGKA/game/menu_game.py
+++
@@ -1,6 +0,0 @@
-def tampilkan_menu_akhir():
-    print("\n1. Main Lagi") #Jika ingin main lagi
-    print("2. Leaderboard") #Untuk melihat leaderboard
-    print("3. Lihat History")   #Melihat history permainan
-    print("4. Lihat Riwayat Pertandingan Angka")    #Melihat riwayat permainan
-    print("5. Kembali ke Menu") #Kembali ke menu utama
